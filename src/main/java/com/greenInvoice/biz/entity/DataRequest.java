021ace371d2187e4202f352c793b1bdea7afdefc
maduraimvr <madurai.mvr@gmail.com>
2017-12-23 16:58:06 +0530

    refs #greenInvoice Documatation Commit

--- src/main/java/com/greenInvoice/biz/entity/DataRequest.java
+++ src/main/java/com/greenInvoice/biz/entity/DataRequest.java
@@ -1,17 +1,50 @@
 package com.greenInvoice.biz.entity;
 
+/**
+ * DataRequest Entity to perform ajax calls.
+ * 
+ * @author Venkatraman
+ *
+ */
 public class DataRequest {
+    /**
+     * value1
+     */
     private Integer value1;
+    
+    /**
+     * value2
+     */
     private Integer value2;
+    
+    /**
+     * 
+     * @return value1
+     */
     public Integer getValue1() {
         return value1;
     }
+    
+    /**
+     * 
+     * @param value1
+     */
     public void setValue1(Integer value1) {
-        this.value1 = value1;
+        this.value1 = value1; 
     }
+    
+    /**
+     * 
+     * @return value2
+     */
     public Integer getValue2() {
         return value2;
     }
+    
+    /**
+     * 
+     * @param value2
+     */
     public void setValue2(Integer value2) {
         this.value2 = value2;
     }
--- src/main/java/com/greenInvoice/front/dashboard/GreenInvoiceDashboardController.java
+++ src/main/java/com/greenInvoice/front/dashboard/GreenInvoiceDashboardController.java
@@ -8,14 +8,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.greenInvoice.biz.entity.DataRequest;
 
+/**
+ * GreenInvoiceDashboardController toa spring based controller.
+ * 
+ * @author Venkatraman
+ *
+ */
 @Controller
 public class GreenInvoiceDashboardController {
 
+    /**
+     * index method to start.
+     * 
+     * @return index
+     */
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return "index";
     }
 
+    /**
+     * controllerMethod to add two numbers.
+     * 
+     * @param request
+     * @return res
+     */
     @RequestMapping(value = "/addNumber", method = RequestMethod.POST)
     @ResponseBody
     public String controllerMethod(@RequestBody DataRequest request) {
