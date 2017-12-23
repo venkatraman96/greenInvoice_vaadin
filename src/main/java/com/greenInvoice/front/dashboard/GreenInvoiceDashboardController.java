1acacb2fa602020e64f75bff89e00024b817a706
maduraimvr <madurai.mvr@gmail.com>
2017-12-23 16:18:02 +0530

    refs #greenInvoice Skelrton commit

---
+++ src/main/java/com/greenInvoice/front/dashboard/GreenInvoiceDashboardController.java
@@ -0,0 +1,30 @@
+package com.greenInvoice.front.dashboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.greenInvoice.biz.entity.DataRequest;
+
+@Controller
+public class GreenInvoiceDashboardController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/addNumber", method = RequestMethod.POST)
+    @ResponseBody
+    public String controllerMethod(@RequestBody DataRequest request) {
+        Integer value1 = request.getValue1();
+        Integer value2 = request.getValue2();
+        System.out.println("values :" + value1 + " , " + value2);
+        int result = value1 + value2;
+        System.out.println(result);
+        String res = Integer.toString(result);
+        return res;
+    }
+}
